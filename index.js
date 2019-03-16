84ca8afbf3739a34b140fc7e1c353681dff98caf
Hari Prasath <imhphari@gmail.com>
2019-03-16 17:29:05 +0530

    v1

--- index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {Text,AppRegistry} from 'react-native';
-
-const App=()=>(
-        <text>Some text</text>
-    );
-
-
-ReactNative.AppRegistry.registerComponent('native-apps',()=>App);
