75af1b8e605669e6fa77b98c968e3272df12ddd8
Hari Prasath <imhphari@gmail.com>
2019-03-16 17:14:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-/**
- * @format
- */
+import React from 'react';
+import {Text,AppRegistry} from 'react-native';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+const App=()=>(
+        <text>Some text</text>
+    );
 
-AppRegistry.registerComponent(appName, () => App);
+
+ReactNative.AppRegistry.registerComponent('native-apps',()=>App);
