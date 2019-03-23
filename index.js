c731dad9d912b8e67cbc19069d8e115d7c55bf11
Hari Prasath <imhphari@gmail.com>
2019-03-23 23:53:18 +0530

    Updated tasks

--- index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {AppRegistry} from 'react-native';
-import Header from './src/components/header';
-import Login from './src/components/login';
-const App=()=>(
-    <Login />
-);
-
-AppRegistry.registerComponent('nativeapp',()=>App);
